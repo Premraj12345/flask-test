1252e89b0e16982629bbf1c39506e5b4f8b5c75a
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2022-08-16 16:33:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ proxies = proxlist.list_proxies()
 valid_proxies = []
 
 while True:
-	for a in proxies:
-		checking = checker.check_proxy(a)
-		if checking['status'] == True:
-			valid_proxies.append(a)
-		else:
-			continue
+    for a in proxies:
+        checking = checker.check_proxy(a)
+	if checking['status'] == True:
+	    valid_proxies.append(a)
+	else:
+	    continue
 	
