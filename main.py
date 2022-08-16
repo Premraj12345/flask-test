bec210011c1e7a61eba7bc5489b914b6b2662c50
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2022-08-16 16:31:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import proxlist
+from proxy_checking import ProxyChecker
+
+checker = ProxyChecker()
+
+proxies = proxlist.list_proxies()
+
+valid_proxies = []
+
+while True:
+	for a in proxies:
+		checking = checker.check_proxy(a)
+		if checking['status'] == True:
+			valid_proxies.append(a)
+		else:
+			continue
+	
