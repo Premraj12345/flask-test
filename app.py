961330b897739527a97f1d9df5dfdce431dd229e
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2022-08-16 18:55:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask
 app = Flask(__name__)
 
 valid_proxies = []
-valid_proxies2 = []
 
 @app.route('/')
 def main():
@@ -17,21 +16,12 @@ def setproxylist(ipport):
 
 @app.route('/proxylist')
 def proxylist():
-    return f'{valid_proxies2}'
+    return f'{valid_proxies}'
 
 @app.route('/clearlist')
 def clearlist():
     valid_proxies.clear()
     return '<h1>Succesfully Cleared valid_proxies List<h1>'
 
-@app.route('/clearlist2')
-def clearlist2():
-    valid_proxies2.clear()
-    return '<h1> Successfully Cleared valid_proxies2 List<h1>'
-
-@app.route('/proxyaddto2')
-def proxyaddto2():
-    valid_proxies2.append(valid_proxies)
-    return '<h1>Succesfully added to valid_proxies2 <h1>'
 if __name__ == '__main__':
     app.run()
