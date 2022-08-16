6b3fe92d8e18fe826a5b7afbb89eabf940e8bf84
Premraj12345 <94243441+Premraj12345@users.noreply.github.com>
2022-08-16 17:17:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from main import valid_proxies2
+
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    img = 'https://raw.githubusercontent.com/Premraj12345/flask-test/main/Inkem.jpg'
-    return f'''
-    <img src="{img}"></img>
-    '''
+    return '<h2> Hello World <h2>'
 @app.route('/proxylist')
 def proxylist2():
     return f'{valid_proxies2}'
